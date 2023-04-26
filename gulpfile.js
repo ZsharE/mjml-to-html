c274ea749053b1851e6bb4f337cbd4d5773c3c89
Stanescu Eduard-Dan <zflashare@gmail.com>
2023-04-26 11:34:58 +0300

    commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const mjml = require('gulp-mjml');
+const mjmlEngine = require('mjml');
+const htmlMinify = require('html-minifier');
+const jsonEditor = require('gulp-json-editor');
+
+let htmlContents = null;
+
+gulp.task('mjml-to-html', function () {
+    return gulp.src('./test.mjml')
+        .pipe(mjml(mjmlEngine))
+        .on('data', function(file) {
+            const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), {
+                collapseWhitespace: true,
+                minifyCSS: true,
+                processConditionalComments: true
+            }));
+            htmlContents = '"' + buferFile.toString() + '"';
+            return file.contents = buferFile;
+        })
+        .pipe(gulp.dest('./html'))
+});
+
+gulp.task('update-json', function () {
+    return gulp.src('./template.json')
+        .pipe(jsonEditor({
+            'html': htmlContents
+        }))
+        .pipe(gulp.dest('./'))
+});
+
+gulp.task('run', gulp.series('mjml-to-html', 'update-json'));
